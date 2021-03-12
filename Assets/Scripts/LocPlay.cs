bda731b138967ce979918efd48985df0d688a5e4
Dan S <dsys313@gmail.com>
2021-03-11 19:05:37 -0500

    New method of marker tracking implemented

---
+++ Assets/Scripts/LocPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocPlay : MonoBehaviour
+{
+    public Image marker;
+    private float x = 0;
+    private float y = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Finds where the location marker is on Mapbox and puts the UI location marker ontop
+        Vector3 pos = this.transform.position;
+        x = pos.x;
+        y = pos.y;
+        marker.transform.position = new Vector2(x,y);
+    }
+}
