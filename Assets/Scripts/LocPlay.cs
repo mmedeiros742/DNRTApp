200a8ab2fa3802ad977a95ea4ad782a8dc7626fd
Dan S <dsys313@gmail.com>
2021-03-26 15:15:26 -0400

    New Branch for Map Testing

--- Assets/Scripts/LocPlay.cs
+++ Assets/Scripts/LocPlay.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class LocPlay : MonoBehaviour
 {
-    public Image marker;
+    public GameObject marker;
     private float x = 0;
     private float y = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* Starts the marker on the gps
+        Vector3 pos = this.transform.position;
+        x = pos.x;
+        y = pos.y;
+        marker.transform.position = new Vector2(x,y);
+        */
     }
 
     // Update is called once per frame
